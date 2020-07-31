79ed9c2dd8f3974d6d4ec9b305762c7c382fe585
Semergal <37205006+Semergal@users.noreply.github.com>
2020-07-31 17:52:21 +1000

    1

---
+++ catalog/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from catalog.views import index, dogs, cats, parrots, contacts, pet
+from django.urls import path
+
+app_name = 'catalog'
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('index.html/', index, name='index'),
+    path('dogs/', dogs, name='dogs'),
+    path('cats/', cats, name='cats'),
+    path('pet/<str:id>', pet, name="pet"),
+    path('parrots/', parrots, name='parrots'),
+    path('contacts/', contacts, name="contacts")
+]
+
